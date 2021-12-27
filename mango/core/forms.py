5650c796f978d0a1b380933b198a36b8e246185f
Matt Duffield <matt.duffield@gmail.com>
2021-12-27 08:19:06 -0800

    Added core folder.

---
+++ mango/core/forms.py
@@ -0,0 +1,281 @@
+import asyncio
+import markupsafe
+from wtforms import SelectFieldBase, widgets
+from wtforms.widgets import TextInput
+from wtforms.fields.core import Field, UnboundField
+from mango.db.api import find, find_one, run_pipeline, find_sync
+from mango.db.query import Query
+from settings import templates, DATABASE_NAME
+
+
+label_class = 'block text-gray-700 text-sm font-light mb-2'
+input_class = '''
+  form-control
+  block
+  w-full
+  px-3
+  py-1.5
+  text-base
+  font-normal
+  text-gray-700
+  bg-white bg-clip-padding
+  border border-solid border-gray-300
+  rounded
+  transition
+  ease-in-out
+  m-0
+  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+'''
+textarea_class = '''
+  form-control
+  block
+  w-full
+  px-3
+  py-1.5
+  text-base
+  font-normal
+  text-gray-700
+  bg-white bg-clip-padding
+  border border-solid border-gray-300
+  rounded
+  transition
+  ease-in-out
+  m-0
+  h-48
+  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+'''
+chk_class = '''
+  form-check-input
+  xappearance-none
+  h-4
+  w-4
+  border
+  border-gray-300
+  rounded-sm
+  bg-white
+  checked:bg-blue-600
+  checked:border-blue-600
+  focus:outline-none
+  transition
+  duration-200
+  mt-1
+  align-top
+  bg-no-repeat
+  bg-center
+  bg-contain
+  float-left
+  mr-2
+  cursor-pointer
+'''
+select_class = '''form-select appearance-none
+  block
+  w-full
+  px-3
+  py-1.5
+  text-base
+  font-normal
+  text-gray-700
+  bg-white bg-clip-padding bg-no-repeat
+  border border-solid border-gray-300
+  rounded
+  transition
+  ease-in-out
+  m-0
+  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'''
+
+
+class DivField(Field):
+
+  def __call__(self, **kwargs):
+    return markupsafe.Markup(f'''
+<div class="mt-2">
+  <div class="mt-2 flex gap-4">
+    <select class="flex-1 {select_class}">
+      <option value="12am">12:00 AM</option>
+      <option value="1am">1:00 AM</option>
+      <option value="2am">2:00 AM</option>
+      <option value="3am">3:00 AM</option>
+      <option value="4am">4:00 AM</option>
+      <option value="5am">5:00 AM</option>
+      <option value="6am">6:00 AM</option>
+      <option value="6am">6:00 AM</option>
+      <option value="7am">7:00 AM</option>
+      <option value="8am" selected="true">8:00 AM</option>
+      <option value="9am">9:00 AM</option>
+      <option value="10am">10:00 AM</option>
+      <option value="11am">11:00 AM</option>
+    </select>
+    <select class="flex-1 {select_class}">
+      <option value="12pm">12:00 PM</option>
+      <option value="1pm">1:00 PM</option>
+      <option value="2pm">2:00 PM</option>
+      <option value="3pm">3:00 PM</option>
+      <option value="4pm">4:00 PM</option>
+      <option value="5pm">5:00 PM</option>
+      <option value="6pm">6:00 PM</option>
+      <option value="6pm">6:00 PM</option>
+      <option value="7pm">7:00 PM</option>
+      <option value="8pm">8:00 PM</option>
+      <option value="9pm">9:00 PM</option>
+      <option value="10pm">10:00 PM</option>
+      <option value="11pm">11:00 PM</option>    
+    </select>
+  </div>
+  <div class="mt-2">
+    <label class="inline-flex items-center">
+      <input type="checkbox" class="form-checkbox {chk_class}" name="sun" value="">
+      <span class="xml-2">Sun</span>
+    </label>
+    <label class="inline-flex items-center ml-4">
+      <input type="checkbox" class="form-checkbox {chk_class}" name="mon" value="">
+      <span class="xml-2">Mon</span>
+    </label>
+    <label class="inline-flex items-center ml-4">
+      <input type="checkbox" class="form-checkbox {chk_class}" name="tue" value="">
+      <span class="xml-2">Tue</span>
+    </label>
+    <label class="inline-flex items-center ml-4">
+      <input type="checkbox" class="form-checkbox {chk_class}" name="wed" value="">
+      <span class="xml-2">Wed</span>
+    </label>
+    <label class="inline-flex items-center ml-4">
+      <input type="checkbox" class="form-checkbox {chk_class}" name="thu" value="">
+      <span class="xml-2">Thu</span>
+    </label>
+    <label class="inline-flex items-center ml-4">
+      <input type="checkbox" class="form-checkbox {chk_class}" name="fri" value="">
+      <span class="xml-2">Fri</span>
+    </label>
+    <label class="inline-flex items-center ml-4">
+      <input type="checkbox" class="form-checkbox {chk_class}" name="sat" value="">
+      <span class="xml-2">Sat</span>
+    </label>
+  </div>
+</div>
+      ''')
+
+  def _value(self):
+    if self.data:
+      return ', '.join(self.data)
+    else:
+      return ''
+
+  def process_formdata(self, valuelist):
+    if valuelist:
+      self.data = [x.strip() for x in valuelist[0].split(',')]
+    else:
+      self.data = []
+
+
+class TagListField(Field):
+  widget = TextInput()
+
+  def _value(self):
+    if self.data:
+      return ', '.join(self.data)
+    else:
+      return ''
+
+  def process_formdata(self, valuelist):
+    if valuelist:
+      self.data = [x.strip() for x in valuelist[0].split(',')]
+    else:
+      self.data = []
+
+
+class WeeklyHoursField(Field):
+  widget = widgets.WeekInput()
+
+  def _value(self):
+      if self.data:
+          return ', '.join(self.data)
+      else:
+          return ''
+
+  def process_formdata(self, valuelist):
+      if valuelist:
+          self.data = [x.strip() for x in valuelist[0].split(',')]
+      else:
+          self.data = []
+
+  # def __init__(self, label=None, validators=None, filters=tuple(),
+  #   description='', id=None, default=None, widget=None,
+  #   render_kw=None, _form=None, _name=None, _prefix='',
+  #   _translations=None, _meta=None):
+  #   super(WeeklyHoursField, self).__init__(label, validators, **kwargs)                 
+  # def __html__(self):
+  #       """
+  #       Returns a HTML representation of the field. For more powerful rendering,
+  #       see the :meth:`__call__` method.
+  #       """
+  #       return '''
+  #       <ul>
+  #         <li>Sun</li>
+  #         <li>Mon</li>
+  #         <li>Tue</li>
+  #         <li>Wed</li>
+  #         <li>Thu</li>
+  #         <li>Fri</li>
+  #         <li>Sat</li>
+  #       </ul>
+  #       '''
+
+
+class QuerySelectField(SelectFieldBase):
+  widget = widgets.Select()
+
+  def __init__(self, label=None, validators=None, 
+               collection=None, query_type='find', query={},
+               projection={'name': 1, 'value': 1, '_id': 0}, 
+               allow_blank=True, blank_text='Choose...', 
+               display_member=lambda data: f'{data.get("name")}', value_member=lambda data: f'{data.get("value")}', **kwargs):
+    super(QuerySelectField, self).__init__(label, validators, **kwargs)
+
+    self.collection = collection
+    self.query =  Query(
+      database=DATABASE_NAME,
+      collection=collection,
+      query_type=query_type,
+      query=query,
+      projection=projection
+    )
+    self.allow_blank = allow_blank
+    self.blank_text = blank_text
+    self.display_member = display_member
+    self.value_member = value_member
+    self._object_list = None
+
+  def _get_data(self):
+    if self._formdata is not None:
+      for pk, obj in self._get_object_list():
+        if pk == self._formdata:
+          self._set_data(obj)
+          break
+    return self._data
+
+  def _set_data(self, data):
+    self._data = data
+    self._formdata = None
+
+  data = property(_get_data, _set_data)
+
+  def _get_object_list(self):
+    if self._object_list is None:
+      self._object_list = self.load_choices()
+
+    return self._object_list
+
+  def iter_choices(self):
+    if self.allow_blank:
+      yield (None, self.blank_text, self.data is None)
+
+    for x in self._get_object_list():
+      yield (self.value_member(x), self.display_member(x), self.value_member(x) == self.data)
+      # yield x  # TODO: Remove
+
+  def load_choices(self):
+    choices = find_sync(self.query)
+    # results = find_sync(self.query)  # TODO: Remove
+    # choices = [(v, v, v == self.data) for x in results for k, v in x.items() if k == 'value']  # TODO: Remove
+    return choices
+
---
+++ mango/core/views.py
@@ -0,0 +1,548 @@
+import json
+from bson import json_util, ObjectId
+from fastapi import APIRouter, Request, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from typing import (
+    Deque, Dict, FrozenSet, List, Optional, Sequence, Set, Tuple, Union
+)
+from mango.db.query import datetime_parser, json_from_mongo, Credentials, Query, QueryOne, Count, InsertOne, InsertMany, Update, Delete, BulkWrite, AggregatePipeline
+from mango.db.api import find, find_one, run_pipeline, delete, update_one, insert_one
+from settings import templates, DATABASE_NAME
+
+from fastapi_router_controller import Controller
+
+
+
+def get_router(prefix: str = '', tags: List[str] = ['Views']):
+  return APIRouter(
+   prefix = prefix,
+    tags = tags
+  )
+
+def get_controller(prefix: str = '', tags: List[str] = ['Views']):
+  router = get_router(prefix=prefix, tags=tags)
+  controller = Controller(router)
+  return controller
+
+
+class StaticView():
+  async def get(self, request: Request):
+    self.request = request
+    context = {'request': request}
+    template_name = self.get_template_name()
+    response = templates.TemplateResponse(template_name, context)
+    return response
+
+  def get_template_name(self):
+    pass
+
+
+class BaseView():
+  '''
+    It depends on a model class and its meta class to provide all the necessary 
+    information required for loading data for create, update, or delete.
+  '''
+  template_name = ''
+  model_name = ''
+  model_name_plural = ''
+  object_display = lambda self, object: f''
+  list_url = ''
+  create_url = ''
+  delete_url = lambda self, object: f''
+  query_type = ''
+
+  def __init__(self):
+    if not self.template_name:
+      raise Exception('Missing attribute template_name!')
+    if bool(hasattr(self, 'model_name') and self.model_name and hasattr(self, 'model_name_plural') and self.model_name_plural):
+      pass
+    else:
+      if not hasattr(self, 'model_class'):
+        raise Exception('Missing attribute model_class!')
+      if not hasattr(self.model_class, 'Meta'):
+        raise Exception(f'Missing attribute Meta on {self.model_class} class!')
+      if not hasattr(self.model_class.Meta, 'model_name'):
+        raise Exception(f'Missing attribute model_name on Meta on {self.model_class} class!')
+      if not hasattr(self.model_class.Meta, 'model_name_plural'):
+        raise Exception(f'Missing attribute model_name_plural on Meta on {self.model_class} class!')
+      self.model_name = self.model_class.Meta.model_name
+      self.model_name_plural = self.model_class.Meta.model_name_plural
+    self.list_url = f'{self.model_name}-list'
+    self.create_url = f'{self.model_name}-create'
+
+  async def get(self, request: Request, _id: str = ''):
+    self.request = request
+    self._id = _id
+    context = await self.get_context_data(request, _id)
+    template_name = self.get_template_name()
+    response = templates.TemplateResponse(template_name, context)
+    return response
+
+  async def post(self, request: Request, _id: str = ''):
+    pass
+
+  def get_template_name(self):
+    pass
+
+  # def set_form(self, form, data):
+  #   if data and form:
+  #     for prop in data:
+  #       if hasattr(form, prop):
+  #         if form[prop].type == 'FormField':
+  #           for sub_prop in data[prop]:
+  #             form[prop].form[sub_prop].data = data[prop][sub_prop]
+  #         else:
+  #           form[prop].data = data[prop]
+  #   return form
+
+  async def get_context_data(self, request, _id: str = ''):
+    context = {'request': request, 'MODEL_NAME_PLURAL': self.model_name_plural, 'MODEL_NAME': self.model_name, 'OBJECT_DISPLAY': self.object_display, 'list_url': self.list_url, 'create_url': self.create_url, 'delete_url': self.delete_url}
+    form = None
+    form = self.form_class(request)
+    if hasattr(self, 'form_class') and self.form_class:
+      form = await self.form_class.from_formdata(request)
+      context['form'] = form
+    if _id:
+      data = await self.get_queryset()
+      if data and form:
+        for prop in data:
+          if hasattr(form, prop):
+            if form[prop].type == 'FormField':
+              for sub_prop in data[prop]:
+                form[prop].form[sub_prop].data = data[prop][sub_prop]
+            else:
+              form[prop].data = data[prop]
+      context['object'] = data
+      context['_id'] = _id
+    else:
+      context['object'] = {}
+    return context
+
+  async def get_queryset(self):
+    query = self.get_query()
+    data = await find_one(query)
+    return data
+
+  def get_query(self):
+    query = Query(
+      database=DATABASE_NAME,
+      collection=self.model_name_plural,
+      query_type=self.query_type,
+      query={'_id': self._id}
+    )
+    return query
+
+  async def get_lookups(self, collection, query_type = 'find', query = {}, projection = {'name': 1, 'value': 1, '_id': 0}):
+    query = Query(
+      database=DATABASE_NAME,
+      collection=collection,
+      query_type=query_type,
+      query=query,
+      projection=projection
+    )
+    lookups = await find(query)
+    return lookups
+
+
+class View():
+  template_name = ''
+  model_name = ''
+  model_name_plural = ''
+  object_display = lambda self, object: f''
+  list_url = ''
+  create_url = ''
+  delete_url = lambda self, object: f''
+  query_type = ''
+
+  def __init__(self):
+    self.list_url = f'{self.model_name}-list'
+    self.create_url = f'{self.model_name}-create'
+
+  async def get(self, request: Request, _id: str = ''):
+    context = await self.get_context_data(request, _id)
+    template_name = self.get_template_name()
+    response = templates.TemplateResponse(template_name, context)
+    return response
+
+  def get_template_name(self):
+    pass
+
+  async def get_context_data(self, request, _id: str = ''):
+    context = {'request': request, 'MODEL_NAME_PLURAL': self.model_name_plural, 'MODEL_NAME': self.model_name, 'OBJECT_DISPLAY': self.object_display, 'list_url': self.list_url, 'create_url': self.create_url, 'delete_url': self.delete_url}
+    if self.form_class:
+      form = await self.form_class.from_formdata(request)
+      context['form'] = form
+    if _id:
+      query = self.get_query(_id)
+      data = await find_one(query)
+      if data and form:
+        for prop in data:
+          if hasattr(form, prop):
+            form[prop].data = data[prop]
+      context['object'] = data
+      context['_id'] = _id
+    return context
+
+  def get_query(self, _id: str):
+    query = Query(
+      database=DATABASE_NAME,
+      collection=self.model_name_plural,
+      query_type=self.query_type,
+      query={'_id': _id}
+    )
+    return query
+
+  async def get_lookups(self, collection, query_type = 'find', query = {}, projection = {'name': 1, 'value': 1, '_id': 0}):
+    query = Query(
+      database=DATABASE_NAME,
+      collection=collection,
+      query_type=query_type,
+      query=query,
+      projection=projection
+    )
+    lookups = await find(query)
+    return lookups
+
+
+class CreateView(BaseView):
+  template_name = 'crud/item/create.html'
+  query_type = 'find'
+  insert_type = 'insert_one'
+
+  def __init__(self):
+    super().__init__()
+
+  def get_template_name(self):
+    if self.request.state.htmx:
+      return 'crud/item/partials/create.html'
+    else:
+      return 'crud/item/create.html'
+
+  async def get(self, request: Request):
+    return await super().get(request=request)
+
+  async def post(self, request: Request):
+    self.request = request
+    form = await self.form_class.from_formdata(request)
+
+    if await form.validate_on_submit():
+      payload = InsertOne(
+        database=DATABASE_NAME,
+        collection=self.model_name_plural,
+        insert_type=self.insert_type,
+        data=form.data,
+      )
+      await insert_one(payload)
+      response = RedirectResponse(url=request.url_for(self.list_url), status_code=status.HTTP_302_FOUND)
+    else:
+      context = {'request': request, 'MODEL_NAME_PLURAL': self.model_name_plural, 'MODEL_NAME': self.model_name, 'OBJECT_DISPLAY': self.object_display, 'list_url': self.list_url, 'create_url': self.create_url, 'delete_url': self.delete_url}
+      context['form'] = form
+      context['object'] = {}
+      template_name = self.get_template_name()
+      response = templates.TemplateResponse(template_name, context)
+
+    return response
+
+
+class UpdateView(BaseView):
+  template_name = 'crud/item/update.html'
+  query_type = 'find_one'
+  update_type = 'update_one'
+
+  def __init__(self):
+    super().__init__()
+
+  def get_template_name(self):
+    if self.request.state.htmx:
+      return 'crud/item/partials/update.html'
+    else:
+      return 'crud/item/update.html'
+
+  async def get(self, request: Request, _id: str):
+    return await super().get(request=request, _id=_id)
+
+  async def post(self, request: Request, _id: str):
+    self.request = request
+    self._id = _id
+    form = await self.form_class.from_formdata(request)
+
+    if not self._id:
+      raise Exception('Missing attribute!')
+
+    if await form.validate_on_submit():
+      payload = Update(
+        database=DATABASE_NAME,
+        collection=self.model_name_plural,
+        update_type=self.update_type,
+        query={'_id': self._id},
+        data={'$set': form.data},
+      )
+      await update_one(payload)
+      response = RedirectResponse(url=request.url_for(self.list_url), status_code=status.HTTP_302_FOUND)
+    else:
+      context = {'request': request, 'MODEL_NAME_PLURAL': self.model_name_plural, 'MODEL_NAME': self.model_name, 'OBJECT_DISPLAY': self.object_display, 'list_url': self.list_url, 'create_url': self.create_url, 'delete_url': self.delete_url}
+      context['form'] = form
+      context['object'] = {'_id': _id, 'name': form.name.data}
+      template_name = self.get_template_name()
+      response = templates.TemplateResponse(template_name, context)
+
+    return response
+
+
+class DeleteView(BaseView):
+  template_name = 'crud/item/delete.html'
+  delete_url = lambda self, model_name_plural, _id: f'/{model_name_plural}/{_id}/delete'
+  delete_type = 'delete_one'
+
+  def __init__(self):
+    super().__init__()
+
+  def get_template_name(self):
+    if self.request.state.htmx:
+      return 'crud/item/partials/delete.html'
+    else:
+      return 'crud/item/delete.html'
+
+  async def get(self, request: Request, _id: str):
+    return await super().get(request=request, _id=_id)
+
+  async def post(self, request: Request, _id: str = ''):
+    self.request = request
+    self._id = _id
+
+    if not self._id:
+      raise Exception('Missing attribute!')
+
+    payload = Delete(
+      database=DATABASE_NAME,
+      collection=self.model_name_plural,
+      delete_type=self.delete_type,
+      query={'_id': self._id},
+    )
+    await delete(payload)
+    response = RedirectResponse(url=request.url_for(self.list_url), status_code=status.HTTP_302_FOUND)
+    return response
+
+
+class ListView():
+  '''
+    This class is optimized to work with Atlas Search. It depends on a model class and 
+    its meta class to provide all the necessary information required for loading data.
+  '''
+  template_name = 'crud/list/list.html'
+  model_name = ''
+  model_name_plural = ''
+  object_display = lambda self, object: f''
+  list_url = ''
+  create_url = ''
+  delete_url = lambda self, object: f''
+  query_type = 'find'
+
+  def __init__(self):
+    if bool(hasattr(self, 'model_name') and self.model_name and hasattr(self, 'model_name_plural') and self.model_name_plural):
+      pass
+    else:
+      if not hasattr(self, 'model_class'):
+        raise Exception('Missing attribute model_class!')
+      if not hasattr(self.model_class, 'Meta'):
+        raise Exception(f'Missing attribute Meta on {self.model_class} class!')
+      if not hasattr(self.model_class.Meta, 'model_name'):
+        raise Exception(f'Missing attribute model_name on Meta on {self.model_class} class!')
+      if not hasattr(self.model_class.Meta, 'model_name_plural'):
+        raise Exception(f'Missing attribute model_name_plural on Meta on {self.model_class} class!')
+      if not hasattr(self.model_class.Meta, 'search_by'):
+        raise Exception(f'Missing attribute search_by on Meta on {self.model_class} class!')
+      if not hasattr(self.model_class.Meta, 'search_index_name'):
+        raise Exception(f'Missing attribute search_index_name on Meta on {self.model_class} class!')
+      self.model_name = self.model_class.Meta.model_name
+      self.model_name_plural = self.model_class.Meta.model_name_plural
+      self.search_by = self.model_class.Meta.search_by
+    self.list_url = f'{self.model_name}-list'
+    self.create_url = f'{self.model_name}-create'
+
+  async def get(self, request: Request, search: Optional[str] = ''):
+    self.request = request
+    self.search = search
+    context = await self.get_context_data(request, search)
+    self.get_table_definition(context)
+    template_name = self.get_template_name()
+    response = templates.TemplateResponse(template_name, context)
+    return response
+
+  def get_template_name(self):
+    if self.request.state.htmx:
+      return 'crud/list/partials/list.html'
+    else:
+      return 'crud/list/list.html'
+
+  def get_table_definition(self, context):
+    pass
+
+  async def get_context_data(self, request, search: Optional[str] = ''):
+    context = {'request': request, 'MODEL_NAME_PLURAL': self.model_name_plural, 'MODEL_NAME': self.model_name, 'OBJECT_DISPLAY': self.object_display, 'list_url': self.list_url, 'create_url': self.create_url, 'delete_url': self.delete_url}
+    context['search'] = search
+    context['object_list'] = await self.get_queryset()
+    return context
+
+  async def get_queryset(self):
+    query = self.get_query()
+    page_size = self.model_class.Meta.page_size
+    # keys = [x for x in self.model_class.schema().get('properties').keys()]
+    # projection = {keys[i]: 1 for i in range(0, len(keys), 1)}
+    # query.projection = projection
+
+    if self.search:
+      pipeline = self.get_pipeline()
+      batch = await run_pipeline(pipeline)
+      data = batch['cursor']['firstBatch']
+    else:
+      query = self.get_query()
+      query.limit = page_size
+      data = await find(query)
+
+    return data
+
+  def get_query(self):
+    query = Query(
+      database=DATABASE_NAME,
+      collection=self.model_name_plural,
+      query_type=self.query_type
+    )
+    return query
+
+  def get_pipeline(self):
+    search_index_name = self.model_class.Meta.search_index_name
+    page_size = self.model_class.Meta.page_size
+    keys = [x for x in self.model_class.Meta.list_fields]
+    projection = {keys[i]: 1 for i in range(0, len(keys), 1)}
+    project = {
+      "_id": {
+        "$toString": "$_id"
+      }
+    }
+    project = project | projection
+    pipeline = AggregatePipeline(
+      database=DATABASE_NAME,
+      aggregate=self.model_name_plural,
+      pipeline=[
+        {
+          "$search": {
+            "index": search_index_name,
+            "text": {
+              "query": self.search,
+              "path": {
+                "wildcard": "*"
+              },
+              "fuzzy": {}
+            }
+          }
+        },
+        {
+          "$project": project
+        },
+        {
+          "$limit": page_size
+        }
+      ],
+      cursor={}
+    )
+    return pipeline
+
+
+class GenericListView():
+  template_name = 'crud/list/list.html'
+  model_name = ''
+  model_name_plural = ''
+  object_display = lambda self, object: f''
+  list_url = ''
+  create_url = ''
+  delete_url = lambda self, object: f''
+  query_type = 'find'
+  search_by = ''
+  page_size = 15
+  list_fields = []
+
+  def __init__(self):
+    if not self.model_name:
+      raise Exception(f'Missing attribute model_name!')
+    if not self.model_name_plural:
+      raise Exception(f'Missing attribute model_name_plural!')
+    if not self.search_by:
+      raise Exception(f'Missing attribute search_by!')
+    if len(self.list_fields) == 0:
+      raise Exception(f'Please provide at least one list_field entry!')
+    self.list_url = f'{self.model_name}-list'
+    self.create_url = f'{self.model_name}-create'
+
+  async def get(self, request: Request, search: Optional[str] = ''):
+    self.request = request
+    self.search = search
+    context = await self.get_context_data(request, search)
+    self.get_table_definition(context)
+    template_name = self.get_template_name()
+    response = templates.TemplateResponse(template_name, context)
+    return response
+
+  def get_template_name(self):
+    if self.request.state.htmx:
+      return 'crud/list/partials/list.html'
+    else:
+      return 'crud/list/list.html'
+
+  def get_table_definition(self, context):
+    pass
+
+  async def get_context_data(self, request, search: Optional[str] = ''):
+    context = {'request': request, 'MODEL_NAME_PLURAL': self.model_name_plural, 'MODEL_NAME': self.model_name, 'OBJECT_DISPLAY': self.object_display, 'list_url': self.list_url, 'create_url': self.create_url, 'delete_url': self.delete_url}
+    context['search'] = search
+    context['object_list'] = await self.get_queryset()
+    return context
+
+  async def get_queryset(self):
+    query = self.get_query()
+
+    pipeline = self.get_pipeline()
+    batch = await run_pipeline(pipeline)
+    data = batch['cursor']['firstBatch']
+
+    return data
+
+  def get_query(self):
+    query = Query(
+      database=DATABASE_NAME,
+      collection=self.model_name_plural,
+      query_type=self.query_type
+    )
+    return query
+
+  def get_pipeline(self):
+    page_size = self.page_size
+    keys = [x for x in self.list_fields]
+    projection = {keys[i]: 1 for i in range(0, len(keys), 1)}
+    project = {
+      "_id": {
+        "$toString": "$_id"
+      }
+    }
+    project = project | projection
+    pipeline = AggregatePipeline(
+      database=DATABASE_NAME,
+      aggregate=self.model_name_plural,
+      # This pipeline is less efficient when dealing with large datasets.
+      # Use Atlas Search instead for more efficient searches.
+      pipeline=[
+        {
+          "$match": {
+            self.search_by: {"$regex" : self.search , "$options" : "i"}
+          },
+        },
+        {
+          "$project": project
+        },
+        {
+          "$limit": page_size
+        }
+      ],
+      cursor={}
+    )
+    return pipeline
