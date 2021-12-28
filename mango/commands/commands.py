a80d59120096f1aa0dd4edea4bc0efe7e499d555
Matt Duffield <matt.duffield@gmail.com>
2021-12-27 19:05:32 -0800

    Added templates to commands.

--- mango/commands/commands.py
+++ mango/commands/commands.py
@@ -146,19 +146,19 @@ async def start_app(app_name: str):
   click.echo(f'Creating app: {app_name}')
   os.mkdir(app_name)
   os.chdir(app_name)
-  with open('templates/forms.template', 'r') as form_file:
+  with open(os.getcwd() / 'templates/forms.template', 'r') as form_file:
     forms = form_file.read()
-    f = open('forms.template', 'w')
+    f = open('forms.py', 'w')
     f.write(forms)
     f.close()
-  with open('templates/models.template', 'r') as model_file:
+  with open(os.getcwd() / 'templates/models.template', 'r') as model_file:
     models = model_file.read()
-    m = open('models.template', 'w')
+    m = open('models.py', 'w')
     m.write(models)
     m.close()
-  with open('templates/views.template', 'r') as view_file:
+  with open(os.getcwd() / 'templates/views.template', 'r') as view_file:
     views = view_file.read()
-    v = open('views.template', 'w')
+    v = open('views.py', 'w')
     v.write(views)
     v.close()
 
@@ -170,9 +170,9 @@ async def start_project(project_name: str):
   click.echo(f'Creating project: {project_name}')
   # os.mkdir(project_name)
   # os.chdir(project_name)
-  with open('settings.template', 'r') as form_file:
+  with open(os.getcwd() / 'settings.template', 'r') as form_file:
     settings = form_file.read()
-    f = open('settings.template', 'w')
+    f = open('settings.py', 'w')
     f.write(settings)
     f.close()
 
