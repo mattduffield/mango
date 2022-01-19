81516a7ca3ae51f731f1c932d1dc7c2340dc5b8e
Matt Duffield <matt.duffield@gmail.com>
2022-01-19 15:44:18 -0800

    Added core models.

--- mango/core/models.py
+++ mango/core/models.py
@@ -4,6 +4,26 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from pydantic import BaseModel
+from typing import (
+    Deque, Dict, FrozenSet, List, Literal, Optional, Sequence, Set, Tuple, Type, Union
+)
+
+class Funcs(BaseModel):
+  __str__: str
+
+class Meta(BaseModel):
+  search_index_name: str
+  search_by: str
+  order_by: List[str]
+  page_size: int
+
+
+class Field(BaseModel):
+  name: str
+  label: str
+  data_type: str
+  default_value: Optional[str]
+  is_active: bool
 
 
 class App(BaseModel):
@@ -13,4 +33,7 @@ class App(BaseModel):
   label_plural: str
   list_url: str
   list_route_name: str
+  fields: List[Field]
+  meta: Meta
+  funcs: Funcs
   is_active: bool
