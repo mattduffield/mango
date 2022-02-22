9e606cc3a47df6ab67a888492ed39e2574daface
Matt Duffield <matt.duffield@gmail.com>
2022-02-22 15:27:18 -0800

    Bumped version number.

--- mango/core/validators.py
+++ mango/core/validators.py
@@ -1,17 +1,40 @@
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 
 class DataRequiredIf(DataRequired):
-  # a validator which makes a field required if
-  # another field is set and has a truthy value
+  """Validator which makes a field required if another field is set and has a truthy value.
 
-  def __init__(self, other_field_name, *args, **kwargs):
+  Sources:
+    - http://wtforms.simplecodes.com/docs/1.0.1/validators.html
+    - http://stackoverflow.com/questions/8463209/how-to-make-a-field-conditionally-optional-in-wtforms
+
+  """
+  field_flags = ('requiredif',)
+
+  def __init__(self, other_field_name, message=None, *args, **kwargs):
     self.other_field_name = other_field_name
-    super(DataRequiredIf, self).__init__(*args, **kwargs)
+    self.message = message
+
+  def __call__(self, form, field):
+    other_field = form[self.other_field_name]
+    if other_field is None:
+      raise Exception('no field named "%s" in form' % self.other_field_name)
+    if bool(other_field.data):
+      super(DataRequiredIf, self).__call__(form, field)
+
+
+class OptionalIfFieldEqualTo(Optional):
+  # a validator which makes a field optional if
+  # another field has a desired value
+
+  def __init__(self, other_field_name, value, *args, **kwargs):
+    self.other_field_name = other_field_name
+    self.value = value
+    super(OptionalIfFieldEqualTo, self).__init__(*args, **kwargs)
 
   def __call__(self, form, field):
     other_field = form._fields.get(self.other_field_name)
     if other_field is None:
       raise Exception('no field named "%s" in form' % self.other_field_name)
-    if bool(other_field.data):
-      super(DataRequiredIf, self).__call__(form, field)
+    if other_field.data == self.value:
+      super(OptionalIfFieldEqualTo, self).__call__(form, field)
