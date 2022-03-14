aa74d183f803978596412efd6c809eb53bd7b444
Matt Duffield <matt.duffield@gmail.com>
2022-03-14 09:45:56 -0700

    Added get_atlas_search_indexes API and View.

--- mango/db_admin/api.py
+++ mango/db_admin/api.py
@@ -9,9 +9,9 @@ MONGODB_PRIVATE_KEY = os.environ.get('MONGODB_PRIVATE_KEY')
 MONGODB_GROUP_ID = os.environ.get('MONGODB_GROUP_ID')
 MONGODB_CLUSTER_NAME = os.environ.get('MONGODB_CLUSTER_NAME')
 HEADERS = {'Content-Type': 'application/json'}
-URL = f'https://cloud.mongodb.com/api/atlas/v1.0/groups/{MONGODB_GROUP_ID}/clusters/{MONGODB_CLUSTER_NAME}/fts/indexes?pretty=true'
 
 def create_atlas_search_index(collection:str, index_name:str):
+  url = f'https://cloud.mongodb.com/api/atlas/v1.0/groups/{MONGODB_GROUP_ID}/clusters/{MONGODB_CLUSTER_NAME}/fts/indexes?pretty=true'
   data = {
     "collectionName": collection,  # model_field
     "database": DATABASE_NAME,
@@ -20,26 +20,11 @@ def create_atlas_search_index(collection:str, index_name:str):
     },
     "name": index_name  # model_field_search
   }
-  r = requests.post(URL, auth=HTTPDigestAuth(MONGODB_PUBLIC_KEY, MONGODB_PRIVATE_KEY), verify=False, data=json.dumps(data), headers=HEADERS)
+  r = requests.post(url, auth=HTTPDigestAuth(MONGODB_PUBLIC_KEY, MONGODB_PRIVATE_KEY), verify=False, data=json.dumps(data), headers=HEADERS)
   # r = requests.post(url, auth=HTTPDigestAuth(PUBLIC_KEY, PRIVATE_KEY), verify=False, json=data)
-
   print(r.status_code)
 
-
-
-# PUBLIC_KEY=spxuglvc
-# PRIVATE_KEY=5158be5b-9907-4cf5-bf6c-3e72bbc08c39
-# GROUP_ID=60f1f429e8541e664146ba6a
-# CLUSTER_NAME=falm-development
-# curl --user "$PUBLIC_KEY:$PRIVATE_KEY" --digest \
-#      --header "Content-Type: application/json" \
-#      --include \
-#      --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/$GROUP_ID/clusters/$CLUSTER_NAME/fts/indexes?pretty=true" \
-#      --data '{
-#          "collectionName": "model_field",
-#          "database": "falm_matt",
-#          "mappings": {
-#              "dynamic": true
-#          },
-#          "name": "model_field_search"
-#        }'
+def get_atlas_search_indexes(collection:str):
+  url = f'https://cloud.mongodb.com/api/atlas/v1.0/groups/{MONGODB_GROUP_ID}/clusters/{MONGODB_CLUSTER_NAME}/fts/indexes/{DATABASE_NAME}/{collection}?pretty=true'
+  r = requests.get(url, auth=HTTPDigestAuth(MONGODB_PUBLIC_KEY, MONGODB_PRIVATE_KEY), verify=False, headers=HEADERS)
+  print(r.status_code)
--- mango/db_admin/views.py
+++ mango/db_admin/views.py
@@ -2,7 +2,7 @@ import json, os
 from fastapi import APIRouter, Depends, HTTPException
 import settings
 from settings import manager
-from mango.db_admin.api import create_atlas_search_index
+from mango.db_admin.api import create_atlas_search_index, get_atlas_search_indexes
 
 router = APIRouter(
   prefix = '/api_admin',
@@ -12,4 +12,9 @@ router = APIRouter(
 @router.post('/create_atlas_search_index')
 async def create_search_index(collection:str, index_name:str, user=Depends(manager)):
   response = create_atlas_search_index(collection=collection, index_name=index_name)
+  return response
+
+@router.get('/get_atlas_search_indexes')
+async def get_search_indexes(collection:str, user=Depends(manager)):
+  response = get_atlas_search_indexes(collection=collection)
   return response
