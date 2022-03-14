f8751bc5f99acb5000994b0e03cf9d32d9d87cc2
Matt Duffield <matt.duffield@gmail.com>
2022-03-13 17:19:45 -0700

    Updated db_admin signature.

--- mango/db_admin/views.py
+++ mango/db_admin/views.py
@@ -2,7 +2,7 @@ import json, os
 from fastapi import APIRouter, Depends, HTTPException
 import settings
 from settings import manager
-from api import create_atlas_search_index
+from mango.db_admin.api import create_atlas_search_index
 
 router = APIRouter(
   prefix = '/api_admin',
@@ -10,6 +10,6 @@ router = APIRouter(
 )
 
 @router.post('/create_atlas_search_index')
-async def find(collection:str, index_name:str, user=Depends(manager)):
+async def create_search_index(collection:str, index_name:str, user=Depends(manager)):
   response = create_atlas_search_index(collection=collection, index_name=index_name)
   return response
