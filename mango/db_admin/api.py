774eca314650ee79ce8b74748b7ac741a9b46e8f
Matt Duffield <matt.duffield@gmail.com>
2022-03-14 11:56:29 -0700

    Added ability to add/drop/list databases and collections

--- mango/db_admin/api.py
+++ mango/db_admin/api.py
@@ -2,13 +2,59 @@ import requests
 from requests.auth import HTTPDigestAuth
 import json
 import os
+from pymongo import MongoClient
+from bson import json_util, ObjectId
 
+DATABASE_CLUSTER = os.environ.get('DATABASE_CLUSTER')
+DATABASE_USERNAME = os.environ.get('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
 DATABASE_NAME = os.environ.get('DATABASE_NAME')
 MONGODB_PUBLIC_KEY = os.environ.get('MONGODB_PUBLIC_KEY')
 MONGODB_PRIVATE_KEY = os.environ.get('MONGODB_PRIVATE_KEY')
 MONGODB_GROUP_ID = os.environ.get('MONGODB_GROUP_ID')
 MONGODB_CLUSTER_NAME = os.environ.get('MONGODB_CLUSTER_NAME')
 HEADERS = {'Content-Type': 'application/json'}
+
+uri = f'mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_CLUSTER}.mongodb.net/{DATABASE_NAME}?retryWrites=true&w=majority'
+client = MongoClient(uri)
+db = client.test
+
+def list_database_names():
+  names = client.list_database_names()
+  return names
+
+def create_database(database:str):
+  db = client[database]
+  collection = 'log'
+  log = db[collection]
+  new_entry = {'msg': f'Database: {database} created!'}
+  result = log.insert_one(new_entry)
+  data = json.loads(json_util.dumps({'acknowledged': result.acknowledged, 'inserted_id': result.inserted_id}))
+  return data
+
+def drop_database(database:str):
+  client.drop_database(database)
+  return {'msg': f'Database: {database} dropped!'}
+
+def list_collection_names(database:str):
+  entity = client[database]
+  names = entity.list_collection_names()
+  return names
+
+def create_collection(database:str, collection:str):
+  db = client[database]
+  entity = db[collection]
+  new_entry = {'msg': f'Collection: {collection} created!'}
+  result = entity.insert_one(new_entry)
+  data = json.loads(json_util.dumps({'acknowledged': result.acknowledged, 'inserted_id': result.inserted_id}))
+  return data
+
+def drop_collection(database:str, collection:str):
+  db = client[database]
+  entity = db[collection]
+  entity.drop()
+  return {'msg': f'Collection: {collection} dropped from database: {database}!'}
+
 
 def create_atlas_search_index(collection:str, index_name:str):
   url = f'https://cloud.mongodb.com/api/atlas/v1.0/groups/{MONGODB_GROUP_ID}/clusters/{MONGODB_CLUSTER_NAME}/fts/indexes?pretty=true'
--- mango/db_admin/views.py
+++ mango/db_admin/views.py
@@ -2,12 +2,51 @@ import json, os
 from fastapi import APIRouter, Depends, HTTPException
 import settings
 from settings import manager
-from mango.db_admin.api import create_atlas_search_index, get_atlas_search_indexes
+from mango.db_admin.api import (
+  list_database_names,
+  create_database,
+  drop_database,
+  list_collection_names,
+  create_collection,
+  drop_collection,
+  create_atlas_search_index, 
+  get_atlas_search_indexes,
+)
 
 router = APIRouter(
   prefix = '/api_admin',
   tags = ['Mongodon Admin']
 )
+
+@router.get('/list_database_names')
+async def get_database_names(user=Depends(manager)):
+  response = list_database_names()
+  return response
+
+@router.post('/create_database')
+async def create_new_database(database:str, user=Depends(manager)):
+  response = create_database(database)
+  return response
+
+@router.post('/drop_database')
+async def delete_database(database:str, user=Depends(manager)):
+  response = drop_database(database)
+  return response
+
+@router.get('/list_collection_names')
+async def get_collection_names(database:str, user=Depends(manager)):
+  response = list_collection_names(database)
+  return response
+
+@router.post('/create_collection')
+async def create_new_collection(database:str, collection:str, user=Depends(manager)):
+  response = create_collection(database, collection)
+  return response
+
+@router.post('/drop_collection')
+async def delete_collection(database:str, collection:str, user=Depends(manager)):
+  response = drop_collection(database, collection)
+  return response
 
 @router.post('/create_atlas_search_index')
 async def create_search_index(collection:str, index_name:str, user=Depends(manager)):
