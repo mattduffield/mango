68dd9edd509bd27c96e1cf0a0d7ef317d39fcc78
Matt Duffield <matt.duffield@gmail.com>
2022-03-15 14:27:58 -0700

    Worked on adjusting the Tab control and adding hover effect to list screens.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mango',
-    version='0.0.200',
+    version='0.0.201',
     author='Matthew Duffield',
     author_email='matt.duffield@gmail.com',
     description='Web framework for Python',
